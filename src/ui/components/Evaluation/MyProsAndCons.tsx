8dce1ec49b2a30412f921904b74b4f8f075a8379
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-07-17 04:44:59 +0900

    refactor: MyProsAndCons.Card 컴포넌트에서 점수 처리 로직 개선 및 조건부 렌더링 추가

--- src/ui/components/Evaluation/MyProsAndCons.tsx
+++ src/ui/components/Evaluation/MyProsAndCons.tsx
@@ -74,53 +74,32 @@ UserInterviewProsAndCons.Card = function MyProsAndConsCard({
   type: "pros" | "cons";
   item: { keyName: string; score: number } | undefined;
 }) {
-  if (!item) {
-    return (
-      <div
-        className={neato(
-          "flex flex-row items-center px-24 py-8 gap-24 bg-white rounded-[12] shadow-custom border-1",
-          "border-primary-600"
-        )}
-      >
-        <Image
-          src={`/icons/evaluation/face/no_${type}.svg`}
-          alt=""
-          width={90}
-          height={90}
-        />
-        <div>
-          <div className="heading-03 @3xl/pac:heading-02">
-            {type === "pros" ? "장점 없음" : "단점 없음"}
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  const scoreInstance = new Score(item.score);
+  const scoreInstance = item ? new Score(item.score) : undefined;
 
   return (
     <div
       className={neato(
         "flex flex-row items-center px-24 py-8 gap-24 bg-white rounded-[12] shadow-custom border-1",
         {
-          "border-green-500": scoreInstance.isGood(),
-          "border-yellow-400": scoreInstance.isNormal(),
-          "border-red-500": scoreInstance.isBad(),
+          "border-green-500": scoreInstance?.isGood(),
+          "border-yellow-400": scoreInstance?.isNormal(),
+          "border-red-500": scoreInstance?.isBad(),
+          "border-primary-600": !scoreInstance,
         }
       )}
     >
       <Image
-        src={`/icons/evaluation/face/${getEnumKeyByValue(scoreInstance.grade)}.svg`}
+        src={`/icons/evaluation/face/${scoreInstance ? getEnumKeyByValue(scoreInstance.grade) : `no_${type}`}.svg`}
         alt=""
         width={90}
         height={90}
       />
       <div>
         <div className="heading-03 @3xl/pac:heading-02">
-          {getEnumValueByKey(item.keyName)}
+          {item && getEnumValueByKey(item.keyName)}
+          {type === "pros" ? "장점 없음" : "단점 없음"}
         </div>
-        <ScoreChip score={item.score} />
+        {item && <ScoreChip score={item.score} />}
       </div>
     </div>
   );
